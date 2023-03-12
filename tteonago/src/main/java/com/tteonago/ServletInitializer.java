24f09d473e5ea0208990faac9cdf0f4cdb92275e
wonuseo <so222234@gmail.com>
2023-03-12 22:42:40 +0900

    test

---
+++ tteonago/src/main/java/com/tteonago/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tteonago;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TteonagoApplication.class);
+	}
+
+}
---
+++ tteonago/src/test/java/com/tteonago/TteonagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tteonago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TteonagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

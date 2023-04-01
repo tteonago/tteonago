ec04b3d7743cde7c3c47e736a87477d0dcf82bc2
kim joo young <unclescorpion@gmail.com>
2023-04-01 23:45:04 +0900

    🔨 Refactor : GlobalExceptionHandler에 defaultException추가

--- tteonago/src/main/java/com/tteonago/exception/GlobalExceptionHandler.java
+++ tteonago/src/main/java/com/tteonago/exception/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.tteonago.exception;
 
 import java.time.format.DateTimeParseException;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -12,7 +11,13 @@ import org.springframework.web.client.HttpClientErrorException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-   
+	
+	@ExceptionHandler(Exception.class)
+	public String DefaultException(Model model) {
+		model.addAttribute("message", "오류");
+		return "pages/error";
+	}	
+	
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public String handleHttpMessageNotReadableException() {
         return "redirect:/error/404";
@@ -26,7 +31,7 @@ public class GlobalExceptionHandler {
     //메소드에 전달된 인수가 잘못되었거나 부적절한 경우에 발생하는 예외
     @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
     public String handleException(Model model) {
-        model.addAttribute("message", "잘못된 호텔요청 입니다.");
+        model.addAttribute("message", "잘못된 요청 입니다.");
         return "pages/error";
     }
 
@@ -40,7 +45,6 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({MissingServletRequestParameterException.class})
     public String handleMissingServletRequestParameterException(MissingServletRequestParameterException e, Model model) {
         model.addAttribute("message", "필수 요청 파라미터가 누락되었습니다.");
-        model.addAttribute("status", HttpStatus.BAD_REQUEST);
         return "pages/error";
     }
 }

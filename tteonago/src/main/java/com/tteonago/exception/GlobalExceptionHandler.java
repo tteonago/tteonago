3dce8c884086c2313fa92eb9b3e0b9c660769356
wonuseo <so222234@gmail.com>
2023-04-02 02:05:02 +0900

    🔨 Refactor : 오류 멘트 수정 및 파라미터 수정

--- tteonago/src/main/java/com/tteonago/exception/GlobalExceptionHandler.java
+++ tteonago/src/main/java/com/tteonago/exception/GlobalExceptionHandler.java
@@ -14,10 +14,28 @@ public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(Exception.class)
 	public String DefaultException(Model model) {
-		model.addAttribute("message", "오류");
+		model.addAttribute("message", "알수없는 오류가 발생하였습니다");
 		return "pages/error";
 	}	
+	 
+	  @ExceptionHandler({DateTimeParseException.class})
+	    public String handleDateTimeParseException(Model model) {
+	        model.addAttribute("message", "유효하지 않은 날짜입니다");
+	        return "pages/error";
+	    }
 	
+    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
+    public String handleException(Model model) {
+        model.addAttribute("message", "요청 값이 유효하지 않습니다");
+        return "pages/error";
+    }
+  
+    @ExceptionHandler({MissingServletRequestParameterException.class})
+    public String handleMissingServletRequestParameterException(Model model) {
+        model.addAttribute("message", "유효하지 않은 요청 입니다");
+        return "pages/error";
+    }
+    
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public String handleHttpMessageNotReadableException() {
         return "redirect:/error/404";
@@ -26,25 +44,5 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(HttpClientErrorException.class)
     public String handleHttpClientBadRequestException() {
         return "redirect:/error/400";
-    }
-    
-    //메소드에 전달된 인수가 잘못되었거나 부적절한 경우에 발생하는 예외
-    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
-    public String handleException(Model model) {
-        model.addAttribute("message", "잘못된 요청 입니다.");
-        return "pages/error";
-    }
-
-    //날짜 에러
-    @ExceptionHandler({DateTimeParseException.class})
-    public String handleDateTimeParseException(Model model) {
-        model.addAttribute("message", "잘못된 날짜 데이터입니다.");
-        return "pages/error";
-    }
-    
-    @ExceptionHandler({MissingServletRequestParameterException.class})
-    public String handleMissingServletRequestParameterException(MissingServletRequestParameterException e, Model model) {
-        model.addAttribute("message", "필수 요청 파라미터가 누락되었습니다.");
-        return "pages/error";
     }
 }

d789ea70e44884bbaeb543f07615f7cf060a538f
wonuseo <so222234@gmail.com>
2023-04-02 22:11:18 +0900

    🔨 Refactor : GlobalExceptionHandler 매세지 수정, yml 분리, gitignore 추가

--- tteonago/src/main/java/com/tteonago/exception/GlobalExceptionHandler.java
+++ tteonago/src/main/java/com/tteonago/exception/GlobalExceptionHandler.java
@@ -18,19 +18,19 @@ public class GlobalExceptionHandler {
 		return "pages/error";
 	}	
 	 
-	  @ExceptionHandler({DateTimeParseException.class})
-	    public String handleDateTimeParseException(Model model) {
-	        model.addAttribute("message", "유효하지 않은 날짜입니다");
-	        return "pages/error";
-	    }
+	@ExceptionHandler(DateTimeParseException.class)
+	public String handleDateTimeParseException(Model model) {
+	     model.addAttribute("message", "유효하지 않은 날짜입니다");
+	     return "pages/error";
+	}
 	
-    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
+    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
     public String handleException(Model model) {
         model.addAttribute("message", "요청 값이 유효하지 않습니다");
         return "pages/error";
     }
   
-    @ExceptionHandler({MissingServletRequestParameterException.class})
+    @ExceptionHandler(MissingServletRequestParameterException.class)
     public String handleMissingServletRequestParameterException(Model model) {
         model.addAttribute("message", "유효하지 않은 요청 입니다");
         return "pages/error";
@@ -45,4 +45,6 @@ public class GlobalExceptionHandler {
     public String handleHttpClientBadRequestException() {
         return "redirect:/error/400";
     }
+
+
 }

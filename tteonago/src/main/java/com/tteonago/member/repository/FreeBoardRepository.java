10a11a396ef1d675313bc5e6dec39defd444e8d4
Junsoo71 <junsoo.lee71@gmail.com>
2023-04-09 18:22:26 +0900

    ✨Feat: 자유게시판 검색 추가

--- tteonago/src/main/java/com/tteonago/member/repository/FreeBoardRepository.java
+++ tteonago/src/main/java/com/tteonago/member/repository/FreeBoardRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface FreeBoardRepository extends JpaRepository<Freeboard, Long> {
     Page<Freeboard> findAllByDeletedIsFalse(Pageable pageable);
 
     Page<Freeboard> findByTitleContaining(String title, Pageable pageable);
+
+    Page<Freeboard> findByContentsContaining(String contents, Pageable pageable);
+
+    Page<Freeboard> findByTitleContainingOrContentsContaining(String keyword, Pageable pageable);
 }
--- tteonago/src/main/java/com/tteonago/member/service/FreeBoardService.java
+++ tteonago/src/main/java/com/tteonago/member/service/FreeBoardService.java
@@ -25,12 +25,12 @@ public class FreeBoardService {
     public Page<Freeboard> getPageFreeBoard(int page, String keyword, String type) {
         if ("C".equals(type)) {
             // TODO 컨텐츠 내용에 키워드 포함되는 레포지토리 추가
-            return freeBoardRepository.findByTitleContaining(keyword, PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "createdDate")));
+            return freeBoardRepository.findByContentsContaining(keyword, PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "createdDate")));
         } else if ("T".equals(type)) {
             return freeBoardRepository.findByTitleContaining(keyword, PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "createdDate")));
         } else {
             // TODO 전체 검색 타이틀, 컨텐츠 둘다 키워드 포함되는 걸로 레포지토리 추가
-            return freeBoardRepository.findByTitleContaining(keyword, PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "createdDate")));
+            return freeBoardRepository.findByTitleContainingOrContentsContaining(keyword, PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "createdDate")));
         }
     }
 

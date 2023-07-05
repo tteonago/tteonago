028f800fa545c2da56fa6a0b74ad71ada62136c7
so222234@gmail.com <>
2023-07-05 14:23:33 +0900

    :sparkles: 메인페이지 컨트롤러의 지역정보 데이터를 Redis에 캐싱.

---
+++ tteonago/src/main/java/com/tteonago/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.tteonago;
+
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.tteonago.hotel.dto.AreaDTO;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.List;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, List<AreaDTO>> areaRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, List<AreaDTO>> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        //redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(List.class));
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(TypeFactory.defaultInstance().constructParametricType(List.class, AreaDTO.class)));
+
+        return redisTemplate;
+    }
+
+}

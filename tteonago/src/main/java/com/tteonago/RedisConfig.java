a84ba82349f8440ab0b113e37c85d0d8b7328dc8
so222234@gmail.com <>
2023-07-21 06:18:54 +0900

    🛠️ Refactoring : 여러가지 직렬화 방법의 테스트 및 문자열로 직렬화 방법 채택.

--- tteonago/src/main/java/com/tteonago/RedisConfig.java
+++ tteonago/src/main/java/com/tteonago/RedisConfig.java
@@ -1,15 +1,17 @@
 package com.tteonago;
 
-import com.fasterxml.jackson.databind.type.TypeFactory;
-import com.tteonago.hotel.dto.AreaDTO;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.util.List;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.tteonago.hotel.dto.AreaDTO;
 
 @Configuration
 public class RedisConfig {
@@ -17,13 +19,35 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, List<AreaDTO>> areaRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, List<AreaDTO>> redisTemplate = new RedisTemplate<>();
-
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        //redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(List.class));
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(TypeFactory.defaultInstance().constructParametricType(List.class, AreaDTO.class)));
-
+        //redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(AreaDTO.class));
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
-
+    
+    @Bean
+    public RedisTemplate<String, Object> areaRedisTemplateV2(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+    
+    @Bean
+    public RedisTemplate<String,String> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String,String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+    
 }
+    
+    
+    
+    
+    
+    
